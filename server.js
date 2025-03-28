b0ebedd73727a999c74cbcc4ed2d1aa044f18180
nightlydevz <nightlydevz@gmail.com>
2025-03-28 18:11:34 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
-const express = require("express");
+import express from "express";
+import axios from "axios";
 
 const app = express();
 const PORT = 3000;
 
-app.get("/check", (req, res) => {
-  res.status(200).send("online");
+app.get("/check", async (_, res) => {
+  try {
+    const response = await axios.get(
+      "https://jsonplaceholder.typicode.com/posts/1"
+    );
+    res
+      .status(200)
+      .send(`Server is online. Sample data: ${response.data.title}`);
+  } catch (error) {
+    res.status(500).send("Error fetching data");
+  }
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://envn.celinaisd.tech:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
